b62c94e2b22d072ac8d71f52b8ee6debc174573d
Zev Oster <zevelijah@gmail.com>
2022-08-02 14:07:57 -0400

    possible

--- src/components/PrivateLibrary.js
+++ src/components/PrivateLibrary.js
@@ -1,9 +1,32 @@
-import { database } from "../firebase"
-import { useAuth } from "../contexts/AuthContext"
-import React from "react"
+import React, { useRef, useState, useEffect } from "react"
+// import { Form, Button, Card, Alert, Container} from "react-bootstrap"
+// import { database } from "../firebase"
+// import { useAuth } from "../contexts/AuthContext"
+// import { Link, useParams } from "react-router-dom"
 
 export default function PrivateLibrary() {
-  var allRef = database.ref('decks/')  
+  // const { currentUser } = useAuth()
+  // const deckNameRef = useRef()
+  // const deckDescriptionRef = useRef()
+  // const deckPrivacyRef = useRef()
+  // const [error] = useState("")
+  // const [deckList, setDeckList] = useState({list: []})
+  // // const { id } = useParams();
+
+
+  // // var deckId = id
+  // var allRef = database.ref('decks/');
+  // useEffect(() => {
+  //   allRef.on('value', snapshot => {
+  //     let d = snapshot.val()
+  //     if (d === undefined) {
+  //       d = {}
+  //     }
+  //     setDeckList(d)
+  //   });
+  //   }, [])
+  // let a = []
+
   return(
     <>
       How do I move forward?
